9767ba7945b02b4f05e0ea03c48d558c4283208e
starburst <kkes30157@gmail.com>
2017-12-01 20:25:27 +0800

    22

---
+++ src/main/amaze/aaaTest.java
@@ -0,0 +1,16 @@
+package amaze;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class aaaTest {
+    @Test
+    void getType() {
+        aaa zz = new aaa(3,4,5);
+        String result = zz.getType();
+        Assert.assertEquals("NO",result);
+    }
+
+}
